5e4f0940caf9b962251167b8e8e5ae82e2c0a263
Yugoslav Empire <yugoslavreich@gmail.com>
2022-02-04 19:10:14 +0800

    change declarations

--- declarations/index.d.ts
+++ declarations/index.d.ts
@@ -1,11 +1,11 @@
 declare module wpromptjs {
   export default function (
     text: string,
-    { raw, questioner }: { raw: boolean; questioner: string }
+    options: { raw: boolean; questioner: string }
   ): Promise<string>;
   export function prompt(
     text: string,
-    { raw, questioner }: { raw: boolean; questioner: string }
+    options: { raw: boolean; questioner: string }
   ): Promise<string>;
   export const raw: true;
 }
